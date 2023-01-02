bb98f5ddb5a57e59b6f7f66be9e763319529800f
Adam Paul <adamjpaul@gmail.com>
2023-01-02 16:11:13 -0500

    set root volume size

--- src/aws.js
+++ src/aws.js
@@ -45,6 +45,7 @@ async function startEc2Instance(label, githubRegistrationToken) {
     SecurityGroupIds: [config.input.securityGroupId],
     IamInstanceProfile: { Name: config.input.iamRoleName },
     TagSpecifications: config.tagSpecifications,
+    BlockDeviceMappings: [{"DeviceName": "/dev/xvda", "EBS": {"VolumeSize": "16"}}]
   };
 
   try {
